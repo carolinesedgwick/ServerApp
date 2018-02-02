582f5d689f479a9d1fdc63e69bc360ee5067ddf9
Caroline Sedgwick <Caroline.Sedgwick@parivedasolutions.com>
2018-02-02 09:49:31 -0600

    Add project files.

---
+++ ServerApp/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class LunchController: Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Lunch> Get()
+        {
+            return new Lunch[]
+            { new Lunch { Name = "Caroline", Time = 11, Duration = 1},
+             new Lunch { Name = "Laura", Time = 11, Duration = 1 },
+             new Lunch { Name = "Natasha", Time = 12, Duration = 1} };
+
+
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ServerApp/Lunch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    public class Lunch
+    {
+        public string Name { get; set; }
+        public int Time { get; set; }
+        public int Duration { get; set; } = 1;
+        public string Color { get; set; } = "#ebf9d1";
+    }
+}
